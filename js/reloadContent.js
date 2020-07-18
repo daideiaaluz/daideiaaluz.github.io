bfe86904e4244f26f24ffe050b4a81e9941f4e31
daideiaaluz <daideiaaluz@gmail.com>
2020-07-18 18:46:19 -0300

    modification

--- js/reloadContent.js
+++ js/reloadContent.js
@@ -3,6 +3,14 @@ function reloadContent(div, link) {
     var z = document.getElementById(div);
     z.setAttribute("w3-include-html", link);
 
+    // prevent automatic roll
+    z.addEventListener("click", function(event){
+        event.preventDefault()
+    });
+    z.removeEventListener("click", function(event){
+        event.preventDefault()
+    });
+
     // Refresh content in div
     includeHTML();
 }
