1028e519cd242d7e8060dfaabd01b29c2f2371e3
daideiaaluz <daideiaaluz@gmail.com>
2020-07-19 20:57:30 -0300

    update

--- js/reloadContent.js
+++ js/reloadContent.js
@@ -2,13 +2,15 @@ function reloadContent(div, link) {
     var z = document.getElementById(div);
     z.setAttribute("w3-include-html", link);
 
-    // Prevent automatic roll
-    document.getElementById("anchor").addEventListener("click", function(event){
-        event.preventDefault()
-    });
-    document.getElementById("anchor").removeEventListener("click", function(event){
-        event.preventDefault()
-    });
+    if (div != "include-content") {
+        // Prevent automatic roll
+        document.getElementById("anchor").addEventListener("click", function(event){
+            event.preventDefault()
+        });
+        document.getElementById("anchor").removeEventListener("click", function(event){
+            event.preventDefault()
+        });
+    }
 
     // Refresh content in div
     includeHTML();
