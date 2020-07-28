afd4fcbfcd6b75374e1a4c78bc3a9380e5bd9c06
daideiaaluz <daideiaaluz@gmail.com>
2020-07-28 10:55:17 -0300

    update

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,31 +1,37 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+var timerHandle;
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+plusSlidesAuto();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    clearTimeout(timerHandle);
+    timerHandle = setTimeout(plusSlidesAuto, 10000);
 }
 
-function plusDivsAut() {
-    showDivs(slideIndex += 1);
+function plusSlidesAuto() {
+    showSlides(slideIndex += 1);
+    timerHandle = setTimeout(plusSlidesAuto, 10000);
 }
 
-function currentDiv(n) {
-    showDivs(slideIndex = n);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    clearTimeout(timerHandle);
+    timerHandle = setTimeout(plusSlidesAuto, 10000);
 }
 
-function showDivs(n) {
+function showSlides(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-white", "");
+        dots[i].className = dots[i].className.replace(" dot-active", "");
     }
-    x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-white";
-    setTimeout(plusDivsAut, 10000);
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " dot-active";
 }
