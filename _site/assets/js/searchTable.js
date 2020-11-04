9bb233d3d7acdccd26b814469eeed565a5ed6767
daideiaaluz <daideiaaluz@gmail.com>
2020-11-03 23:33:37 -0300

    update paremeters, change img of slideshow and implementation of collapse in page profissionais

--- _site/assets/js/searchTable.js
+++ _site/assets/js/searchTable.js
@@ -1,18 +1,24 @@
 function searchTable() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td, i, txtValue, flag;
     input = document.getElementById("inputSearchTable");
     filter = input.value.toUpperCase();
     table = document.getElementById("tablepress-1");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+    for (i = 1; i < tr.length; i++) {
+        for (j = 0; j < 6; j++) {
+            td = tr[i].getElementsByTagName("td")[j];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    flag = 1;
+                }
             }
+        }
+        if (flag != 1) {
+            tr[i].style.display = "none";
+        }else{
+            flag = 0;
         }
     }
 }
